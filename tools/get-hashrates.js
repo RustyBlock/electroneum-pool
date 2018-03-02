b8c6607c11c28fb4a7a3b7e4e0ade1ba02e3c343
Rusty Block <cryptoharbour@gmail.com>
2018-03-02 00:11:56 +0000

    history of miner hashrate added

---
+++ tools/get-hashrates.js
@@ -0,0 +1,13 @@
+require('../lib/configReader.js');
+require('../lib/logger.js');
+
+var redis = require('redis');
+global.redisClient = redis.createClient(config.redis.port, config.redis.host, {auth_pass: config.redis.auth});
+
+var api = require('../lib/api');
+api.getHashrateHistory('etnkNn3izXrXazWDgZo2LYECBeyTDmJxqedxtFsRNP1hb5znsr36dxp8EPz4svfGAXXMTkHJ7rGHFJYSFhVRSVUY8WJ3zzKJrQ',
+function(rates) {
+    console.log(rates.toString());
+}, function(error) {
+    console.error(error.toString());
+});
