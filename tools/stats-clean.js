81f8a5ccb9e8034431d8c378099a64676fb64063
RustyBlock <admin@rustyblock.com>
2018-07-23 22:57:31 +0100

    stats clean logic

--- tools/stats-clean.js
+++ tools/stats-clean.js
@@ -8,4 +8,20 @@ var logSystem = 'cleanup';
 require('../lib/exceptionWriter.js')(logSystem);
 
 var clean = require("../lib/active-stats");
-clean.cleanUp();
+setTimeout(function() {
+    clean.cleanUp();
+}, 100);
+
+// Delete stats that are older than 1 month
+var max = Math.ceil((new Date()).getTime() / 1000) - (60 * 60 * 24 * 31);
+log('info', logSystem, 'Clearing stats up to %s', [max]);
+
+['blockReward', 'blockTimings', 'networkDiff', 'poolHashrate', 'poolMiners'].forEach(function(key) {
+    global.redisClient.zremrangebyscore(config.coin + ':stats:' + key, '-inf', max, function(error, result) {
+        if(error) {
+            log('error', logSystem, 'Error clearing %s: %s', [key, error]);
+        } else {
+            log('info', logSystem, '%s items of %s list removed', [result, key]);
+        }    
+    });
+});
