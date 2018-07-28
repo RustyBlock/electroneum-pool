d9c0e2913f1b388bd53d27d9b3df128fb6d44471
RustyBlock <admin@rustyblock.com>
2018-07-29 00:18:06 +0100

    don't commit auth file

--- pool-web/redirect.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(req, res) {
-    var host = req.get('Host').toLowerCase();
-    if (host.indexOf('.rustylock.club') > 0 || req.get('X-Forwarded-Proto') !== 'https' || 
-        host.indexOf('www') !== 0) {
-      return res.redirect(301, 'https://www.etn.rustyblock.com' + req.originalUrl);
-    }    
-}
--- pool-web/static/config.js
+++ pool-web/static/config.js
@@ -4,7 +4,7 @@ var blockchainExplorer = "https://blockexplorer.electroneum.com/block/";
 var transactionExplorer = "https://blockexplorer.electroneum.com/tx/";
 
 var isHttps = location.protocol === 'https:';
-var api = (isHttps ? 'https' : 'http') + "://api." + poolHost;
-//var api = 'http://localhost:8117';
+//var api = (isHttps ? 'https' : 'http') + "://api." + poolHost;
+var api = 'http://dev.rustyblock.com:8117';
 var coinUnits = 100;
 var coinDecimalPlaces = 2;
