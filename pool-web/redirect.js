936e46673fec389453edf5493c0a5f357f0454a1
RustyBlock <29672370+RustyBlock@users.noreply.github.com>
2018-08-03 10:46:08 +0100

    Update redirect.js

--- pool-web/redirect.js
+++ pool-web/redirect.js
@@ -1,7 +1,16 @@
 module.exports = function(req, res) {
     var host = req.get('Host').toLowerCase();
-    if (host.indexOf('.rustylock.club') > 0 || req.get('X-Forwarded-Proto') !== 'https' || 
+    // redirect old domain to new and all to HTTPS
+    if(host.indexOf('.rustylock.club') > 0 || req.get('X-Forwarded-Proto') !== 'https' || 
         host.indexOf('www') !== 0) {
       return res.redirect(301, 'https://www.etn.rustyblock.com' + req.originalUrl);
-    }    
+    }
+    // fix URL format broken by automated newsletters - they add '?...' after '#...'
+    if(req.originalUrl.indexOf('?') && req.originalUrl.substr(1,1) === '#') {
+        var parts = req.originalUrl.substr(1).split('?');
+        if(parts.length != 2) { // multiple question marks in the URL, don't know what to do with it
+            return;
+        }
+        return res.redirect(301, 'https://www.etn.rustyblock.com/?' + parts[1] + parts[0]);
+    }
 }
