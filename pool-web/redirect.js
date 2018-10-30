fe505c3888dbbfddfe5fae9fbe0bc2b1b3f201ad
RustyBlock <admin@rustyblock.com>
2018-10-30 22:44:46 +0000

    also redirect www.rutyblock urls

--- pool-web/redirect.js
+++ pool-web/redirect.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next) {
     var host = req.get('Host').toLowerCase();
     // redirect old domain to new and all to HTTPS
     if(host.indexOf('.rustylock.club') > 0 || req.get('X-Forwarded-Proto') !== 'https' || 
-        host.indexOf('www') !== 0) {
+        host.indexOf('www.rustyblock.') === 0) {
       return res.redirect(301, 'https://www.etn.rustyblock.com' + req.originalUrl);
     }
     return next();
